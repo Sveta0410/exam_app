a26dccb0ef5a29f274a12aff867b92ce1a54f5ca
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2024-06-19 13:41:18 +0300

    красивая страничка входа с выпадающим списком

--- 1front/src/Login.jsx
+++ 1front/src/Login.jsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from "react"
 import { Link, useNavigate } from "react-router-dom";
-import { Button, message } from 'antd';
+import { Button, message, Select, Form, Input, Typography  } from 'antd';
+const { Title } = Typography;
 import axios from "axios";
 
 export const Login = () => {
     const [fio, setFio] = useState('')
     const [pass, setPassword] = useState('')
     const [error, setError] = useState('')
+    const [allFIO, setAllFIO] = useState([])
 
 
     const navigate = useNavigate()
@@ -14,23 +16,20 @@ export const Login = () => {
     const [messageApi, contextHolder] = message.useMessage();
 
     const validateForm = () => {
-        if (!pass && !fio ){
-            setError("Введите ФИО и пароль")
-            return false;
-            }
-        else if (!pass){
-            setError("Введите пароль")
-            return false;
-            }
-        else if (!fio ){
-            setError("Введите ФИО")
-            return false;
-            }
         setError("")
+        if (!pass || !fio ){
+            return false;
+            }
         return true;
         }
 
-
+const onChange = (value) => {
+    setFio(value)
+  console.log(`selected ${value}`);
+};
+const onSearch = (value) => {
+  console.log('search:', value);
+};
 //     const sendLoginInfo = () => {
 //         setDisabled(false);
 //         setButtonDisabled(false)
@@ -39,81 +38,131 @@ export const Login = () => {
 //         setAnsBlock(<p></p>)
 //     };
 
-async function sendLoginInfo() {
+    async function fetchFIO() {
+        const all_fio = await axios({
+            method: "get",
+            url: "http://127.0.0.1:8000/get_all_users_fio",
+        }).then(res =>  res.data);
+        console.log("all_fio", all_fio);
+        for (var i=0; i < all_fio.length; i++){
+            all_fio[i] = {value: all_fio[i]["fio"], label: all_fio[i]["fio"]}
+        }
+        console.log("all_fio", all_fio);
+        setAllFIO(all_fio);
+    }
+
+
+    async function sendLoginInfo() {
         if (!validateForm()) return;
+
         const formDetails = new URLSearchParams();
         formDetails.append('username', fio);
         formDetails.append('password', pass);
         try { const response = await fetch("http://127.0.0.1:8000/login/", {
             method: "POST",
-
             headers: {
                 'Content-type': 'application/x-www-form-urlencoded'
-             },
+            },
             body: formDetails,
-        });
-        console.log("erivgierjnvgibjfrtbiwe4bft", response);
-        if (response.ok) {
-            const data = await response.json()
-            console.log("data", data);
-            localStorage.setItem('token', data.access_token);
-            navigate('/exam')
+            });
+            console.log("erivgierjnvgibjfrtbiwe4bft", response);
+            if (response.ok) {
+                const data = await response.json()
+                console.log("data", data);
+                localStorage.setItem('token', data.access_token);
+                navigate('/exam')
 
-         } else {
-             const errorData = await response.json()
-             console.log("errorData", errorData);
-             setError("Неверный пароль или фио")
-//              return  messageApi.info('Пожалуйста, выберите один из вариантов ответа')
-
-             }
-         }catch (error){
-             setError("ошибка")
-             }
+            } else {
+                 const errorData = await response.json()
+                 console.log("errorData", errorData);
+                 setError("Неверный пароль или фио")
+            }
+        }catch (error){
+            setError("ошибка")
+        }
     }
 
-// const aa = () => {
-//
-//     }
+const onFinish = (values) => {
+    setPassword(values.password)
+    setFio(values.fio)
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+    useEffect(() => {
+        fetchFIO()
+    }, []);
+
     return (
         <>
-        <p></p>
-        <center><h2>Вход</h2></center>
-        <form className="form_login">
-            <div className="input-box">
-                <label htmlFor="fio">ФИО </label>
-                <input type="text" onChange={(e)=>setFio(e.target.value)} placeholder="Иванов Иван Иванович" id="fio" name="fio"/>
-            </div>
-            <div className="input-box">
-                <label htmlFor="password">Пароль </label>
-                <input value={pass} onChange={(e)=>setPassword(e.target.value)} type="password" id="password" name="password"/>
-            </div>
-{/*             <button> Войти </button> */}
-            <Button type="primary" onClick={sendLoginInfo} style={{ marginTop: 16 }} >
-                Начать экзамен
-            </Button>
+        <Title align="center">Вход</Title>
+        <center><Form
+        align="center"
+    name="basic"
+//     labelCol={{
+//       span: 10,
+//     }}
+//     wrapperCol={{
+//       span: 15,
+//     }}
+    style={{
+      maxWidth: 500,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="ФИО"
+      name="fio"
+      rules={[
+        {
+          required: true,
+          message: 'Введите ФИО',
+        },
+      ]}
+    >
+    <Select
+    showSearch
+    placeholder="Иванов Иван Иванович"
+    optionFilterProp="label"
+    onChange={onChange}
+    onSearch={onSearch}
+    options={allFIO}
+  />
+    </Form.Item>
 
-            {error && <p style={{color:'red'}}>{error}</p>}
+    <Form.Item
+      label="Пароль"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Введите пароль',
+        },
+      ]}
+    >
+      <Input.Password onChange={(e)=>setPassword(e.target.value)}/>
+    </Form.Item>
 
-{/*             <div className="register-link"> */}
-{/*                 <p>  <a href="#">Создать нового пользователя</a></p> */}
-{/*             </div> */}
+    <Form.Item
+//       wrapperCol={{
+//         offset: 8,
+//         span: 16,
+//       }}
+    >
+      <Button type="primary" htmlType="submit" onClick={sendLoginInfo} >
+        Начать экзамен
+      </Button>
+    </Form.Item>
+  </Form></center>
 
-            </form>
+            <center>{error && <p style={{color:'red'}}>{error}</p>}</center>
             <Button  type="link" href="/" style={{ marginTop: 16 }} >
                     Назад
             </Button>
-{/*         <br /> */}
-{/*         <ul> */}
-{/*                 <li> */}
-{/*                     Endpoint to route to Home component */}
-{/*                     <Link to="/">Home</Link> */}
-{/*                 </li> */}
-{/*                 <li> */}
-{/*                     Endpoint to route to About component */}
-{/*                     <Link to="/exam">exam</Link> */}
-{/*                 </li> */}
-
-{/*             </ul> */}
             </>
         )
     }
